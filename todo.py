633d6ac77d26d60ef6522ae9556dc2b8bb66fc1c
juniorcriste <informaticode@gmail.com>
2025-01-18 16:41:31 -0300

    ToDo Axis 0.2

--- todo.py
+++ todo.py
@@ -3,16 +3,16 @@ import flet as ft
 class ToDo:
     def __init__(self, page: ft.Page):
         self.page = page
-        self.page.bgcolor = ft.colors.YELLOW
+        self.page.bgcolor = ft.colors.WHITE
         self.page.window_width = 200
         self.page.window_height = 200
-        self.page.window_resizable = False
-        self.page.window_always_on_top = True
+        self.page.window_resizable = True
+        self.page.window_always_on_top = False
         self.page.title = 'ToDo Axis' 
         self.main_page()
 
     def main_page(self): 
-        input_task = ft.TextField(hint_text="Digite uma tarefa")
+        input_task = ft.TextField(hint_text='Digite uma tarefa', expand=True) 
 
         input_bar = ft.Row(
             controls=[
