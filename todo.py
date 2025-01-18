5b42a2578b0a938dad6d7b4d217bee7c00444dc1
juniorcriste <informaticode@gmail.com>
2025-01-18 16:48:20 -0300

    ToDo Axis 0.3

--- todo.py
+++ todo.py
@@ -4,8 +4,8 @@ class ToDo:
     def __init__(self, page: ft.Page):
         self.page = page
         self.page.bgcolor = ft.colors.WHITE
-        self.page.window_width = 200
-        self.page.window_height = 200
+        self.page.window_width = 380
+        self.page.window_height = 450
         self.page.window_resizable = True
         self.page.window_always_on_top = False
         self.page.title = 'ToDo Axis' 
@@ -21,7 +21,16 @@ class ToDo:
             ]
         )
 
-        self.page.add(input_bar)
+        tabs = ft.Tabs(
+            selected_index=0,
+            tabs=[
+                ft.Tab(text='Todas tarefas'),
+                ft.Tab(text='Em andamento'),
+                ft.Tab(text='Finalizados')
+            ]
+        )
+
+        self.page.add(input_bar, tabs)
 
 ft.app(target = ToDo)
 
